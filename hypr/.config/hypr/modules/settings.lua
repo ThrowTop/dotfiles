98f94d95182eeff49348aea348321c4aa9a97590
ThrowTop <ThrowTop@users.noreply.github.com>
2026-04-22 19:41:00 +0200

    sync dotfiles 2026-04-22

--- hypr/.config/hypr/modules/settings.lua
+++ hypr/.config/hypr/modules/settings.lua
@@ -1,6 +1,6 @@
 local M = {
     mainMod = "SUPER",
-    terminal = "foot",
+    terminal = "kitty",
     browser = "brave",
     hscripts = os.getenv("HOME") .. "/.config/hypr/scripts",
     is_laptop = io.open("/sys/class/power_supply/BAT1") ~= nil,
